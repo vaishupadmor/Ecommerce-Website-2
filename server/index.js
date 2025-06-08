d87183005902a5a96dd79e87f3c10fbe3843b532
vaishupadmor <vaishupadmor@gmail.com>
2025-06-08 20:11:00 +0530

    added

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(
     cors({
       
-      origin:["http://localhost:3000","https://ecommerce-website-2-client.onrender.com/"],
+      origin:["https://ecommerce-website-2-client.onrender.com"],
       credentials: true,
       allowedHeaders: ["Content-Type", "Authorization"],
        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
